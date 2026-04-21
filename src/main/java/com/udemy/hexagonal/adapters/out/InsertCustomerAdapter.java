15345de2a996d559b1999e893a9863afdbc5d3ad
Gilmar F <gilmaresende>
2026-04-21 16:48:45 -0300

    adaptador de inserção de cliente

---
+++ src/main/java/com/udemy/hexagonal/adapters/out/InsertCustomerAdapter.java
@@ -0,0 +1,25 @@
+package com.udemy.hexagonal.adapters.out;
+
+import com.udemy.hexagonal.adapters.out.repository.CustomerRepository;
+import com.udemy.hexagonal.adapters.out.repository.mapper.CustomerEntityMapper;
+import com.udemy.hexagonal.application.core.domain.Customer;
+import com.udemy.hexagonal.application.ports.out.InsertCustomerOutputPort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InsertCustomerAdapter implements InsertCustomerOutputPort {
+
+    private final CustomerRepository customerRepository;
+
+    private final CustomerEntityMapper customerEntityMapper;
+
+    public InsertCustomerAdapter(CustomerRepository customerRepository, CustomerEntityMapper customerEntityMapper) {
+        this.customerRepository = customerRepository;
+        this.customerEntityMapper = customerEntityMapper;
+    }
+
+    @Override
+    public void insert(Customer customer) {
+        customerRepository.save(customerEntityMapper.toCustomerEntity(customer));
+    }
+}
