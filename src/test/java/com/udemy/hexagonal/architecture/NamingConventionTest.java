dbf6b8a7698ed539af06b4cc0a02553d83669a17
Gilmar F <gilmaresende>
2026-04-25 16:54:31 -0300

    criando teste que garrate classes nos pacotes certos

---
+++ src/test/java/com/udemy/hexagonal/architecture/NamingConventionTest.java
@@ -0,0 +1,33 @@
+package com.udemy.hexagonal.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packages = "com.udemy.hexagonal")
+public class NamingConventionTest {
+
+    @ArchTest
+    public static final ArchRule consumer_reside_only_consumer_package = classes()
+            .that()
+            .haveNameMatching(".*Consumer")
+            .should()
+            .resideInAPackage("..adapters.in.consumer")
+            .as("Class with suffix Consumer should reside in package ..adapters.in.consumer");
+
+
+    @ArchTest
+    public static final ArchRule mapper_reside_only_consumer_package = classes()
+            .that()
+            .haveNameMatching(".*Mapper")
+            .should()
+            .resideInAnyPackage("..adapters.in.consumer.mapper",
+                    "..adapters.in.controller.mapper",
+                    "..adapters.out.client.mapper",
+                    "..adapters.out.repository.mapper"
+            )
+            .as("Class with suffix Mapper should reside in package ..adapters.in.consumer.mapper, ..adapters.in.controller.mapper, ..adapters.out.client.mapper or ..adapters.out.repository.mapper");
+
+}
