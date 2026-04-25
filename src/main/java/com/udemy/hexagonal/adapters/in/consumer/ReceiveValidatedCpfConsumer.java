2539d86a014317b9812ecf8ccc8aba8d30ec9557
Gilmar F <gilmaresende>
2026-04-25 14:20:21 -0300

    criando consumer kafka

---
+++ src/main/java/com/udemy/hexagonal/adapters/in/consumer/ReceiveValidatedCpfConsumer.java
@@ -0,0 +1,23 @@
+package com.udemy.hexagonal.adapters.in.consumer;
+
+import com.udemy.hexagonal.adapters.in.consumer.mapper.CustomerMessageMapper;
+import com.udemy.hexagonal.adapters.in.consumer.message.CustomerMessage;
+import com.udemy.hexagonal.application.ports.in.UpdateCustomerInputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReceiveValidatedCpfConsumer {
+
+    @Autowired
+    private UpdateCustomerInputPort updateCustomerInputPort;
+
+    @Autowired
+    private CustomerMessageMapper customerMessageMapper;
+
+    @KafkaListener(topics = "tp-cpf-validated", groupId = "udemy-kafka")
+    public void receive(CustomerMessage message) {
+        updateCustomerInputPort.update(customerMessageMapper.toCustomer(message), message.getZipCode());
+    }
+}
